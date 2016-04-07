57eaf3b1cde0abc56542847112c38e60149f4478
ChuanhuaLiu <947344529@qq.com>
2016-04-07 13:17:35 +0800

    1

--- mouseevent/game.ts
+++ mouseevent/game.ts
@@ -1,13 +1,38 @@
-
+var human = new render.DisplayObjectContainer();
+human.x = -50;
+human.y = -100;
 var humanContainer = new render.DisplayObjectContainer();
+humanContainer.addChild(human);
+
 var head = new render.Bitmap();
-head.x = 100;
-head.source = "wander-icon.jpg";
-humanContainer.addChild(head);
+head.source = "head.png";
+human.addChild(head);
+
+var trunk = new render.Bitmap();
+trunk.source = "trunk.png";
+human.addChild(trunk);
+
+var left_arm = new render.Bitmap();
+left_arm.source = "left_arm.png";
+human.addChild(left_arm);
+
+var right_arm = new render.Bitmap();
+right_arm.source = "right_arm.png";
+human.addChild(right_arm);
+
+var left_leg = new render.Bitmap();
+left_leg.source = "left_leg.png";
+human.addChild(left_leg);
+
+var right_leg = new render.Bitmap();
+right_leg.source = "right_leg.png";
+human.addChild(right_leg);
 
 
 var renderCore = new render.RenderCore();
-renderCore.start(humanContainer, ["wander-icon.jpg"]);
+
+renderCore.start(humanContainer, ["left_arm.png","right_arm.png", "left_leg.png", "right_leg.png", "head.png", "trunk.png"]);
+
 
 class HumanBody extends Body {
     
@@ -16,14 +41,17 @@ class HumanBody extends Body {
     
 
     onTicker(duringTime: number) {
-        this.x = 100;//+= duringTime * this.vx;
+        this.x += this.vx * duringTime;
         this.y = 100;
+        this.rotation += Math.PI*duringTime;
 
     }
 }
 
 var ticker = new Ticker();
 var body = new HumanBody(humanContainer);
+body.x = 200;
+body.y = 100; 
 ticker.start([body]);
 
 
